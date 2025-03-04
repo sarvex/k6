a4ee293e7dc03edb6aacf4a6445529f27fa7bef0
Federico Tedin <federicotedin@gmail.com>
2025-03-04 15:23:43 +0100

    v2.0

--- repl.js
+++ repl.js
@@ -16,26 +16,40 @@ export const options = {
 };
 
 export default async function () {
-    var _ = undefined;
-    var g = global;
+    const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
+
+    // We need to copy these over to the global context otherwise they
+    // are not accessible from the newly created AsyncFunction below.
+    global.http = http
+    global.browser = browser
+    global.sleep = sleep
 
     while (true) {
         try {
             var input = read_stdin("> ");
 
             // Special syntax: '%set foo = 123' actually evaluates
-            // 'g.foo = 123', which sets the value in globals.
+            // 'global.foo = 123', which sets the value in globals.
             // The value can then be referenced by just using 'foo',
             // as if we had run 'var foo = 123'.
             if (input.startsWith("%set ")) {
-                input = "g." + input.substring(4).trim();
+                input = "global." + input.substring(4).trim();
             }
 
-            // Need to do this in order to allow using 'await'.
-            var code = `(async function() { return ${input} })`;
+            var fn = undefined;
+            try {
+                // Input was an expression
+                fn = AsyncFunction("return " + input)
+            } catch (error) {
+                // Input was a statement
+                fn = AsyncFunction(input)
+            }
 
-            var result = await eval(code)();
-            _ = result;
+            var result = await fn();
+
+            // Easily access the last expression result with '_'.
+            global._ = result;
+
             if (result !== undefined && result !== null) {
                 console.log(result.toString());
             }
@@ -43,7 +57,6 @@ export default async function () {
             console.error(error.toString());
             input = input.trim();
             if (input.startsWith("let") || input.startsWith("const") || input.startsWith("var")) {
-                console.info("Hint: the REPL only supports expressions (e.g. `1 + 1`), not statements (e.g. `var foo = 123`).");
                 console.info("Hint: In order to set a variable globally, use `%foo = 123`.");
             }
         }
